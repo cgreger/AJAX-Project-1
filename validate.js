5dac0b90e9187e924072d58bd0b05f6aef91696a
Chelsea Greger <greger@madisoncollege.edu>
2017-01-29 02:54:00 -0600

    Added js form vars and created stubs for outputDetails(), createErrorMessage(element), removeErrorMessage(element), and isEmpty(element)

---
+++ validate.js
@@ -0,0 +1,51 @@
+function validate() {
+
+	var isValid = true;
+	var name = getElementById("name");
+	var email = getElementById("email");
+	var phoneAreaCode = getElementById("phoneAreaCode");
+	var phonePrefix = getElementById("phonePrefix");
+	var phoneSuffix = getElementById("phoneSuffix");
+	var address = getElementById("address");
+	var city = getElementById("city");
+	var state = getElementById("state");
+	var zip = getElementById("zip");
+	var gender = getElementById("gender");
+	var courses = getElementById("courses");
+
+
+
+
+}
+
+function outputDetails() {
+
+
+
+}
+
+function createErrorMessage(element) {
+
+
+
+}
+
+function removeErrorMessage(element) {
+
+
+
+}
+
+function isEmpty(element) {
+
+	if (element.value.length > 0) {
+
+		return false;
+
+	} else {
+
+		return true;
+
+	}
+
+}
